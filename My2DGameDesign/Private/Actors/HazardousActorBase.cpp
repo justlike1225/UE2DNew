0f1360f8421860199ce6ba12dfaf1e858af64972
Jensure <2153601191@qq.com>
2025-04-28 23:38:30 +0800

    陷阱

---
+++ My2DGameDesign/Private/Actors/HazardousActorBase.cpp
@@ -0,0 +1,167 @@
+﻿
+
+
+#include "Actors/HazardousActorBase.h"
+
+#include "Components/BoxComponent.h" 
+#include "Components/CapsuleComponent.h"
+#include "Components/SphereComponent.h"
+#include "PaperSpriteComponent.h"
+#include "Actors/PaperZDCharacter_SpriteHero.h" 
+#include "Interfaces/Damageable.h"             
+#include "TimerManager.h"
+#include "Engine/World.h"
+
+
+AHazardousActorBase::AHazardousActorBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false; 
+
+	
+	CollisionComp = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionComp"));
+	if (CollisionComp) 
+	{
+		RootComponent = CollisionComp; 
+
+		
+		CollisionComp->SetCollisionProfileName(TEXT("OverlapOnlyPawn")); 
+		CollisionComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly); 
+		CollisionComp->SetGenerateOverlapEvents(true);
+		CollisionComp->SetSimulatePhysics(false); 
+		CollisionComp->CanCharacterStepUpOn = ECB_No;
+
+		
+		CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AHazardousActorBase::OnHazardOverlapBegin);
+		CollisionComp->OnComponentEndOverlap.AddDynamic(this, &AHazardousActorBase::OnHazardOverlapEnd);
+	}
+
+	
+	SpriteComp = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("SpriteComp"));
+	if (SpriteComp && RootComponent) 
+	{
+		SpriteComp->SetupAttachment(RootComponent);
+		SpriteComp->SetCollisionEnabled(ECollisionEnabled::NoCollision); 
+	}
+}
+
+
+void AHazardousActorBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AHazardousActorBase::OnHazardOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    
+	APaperZDCharacter_SpriteHero* PlayerHero = Cast<APaperZDCharacter_SpriteHero>(OtherActor);
+
+    
+	if (PlayerHero && !OverlappingHeroPtr.IsValid())
+	{
+		OverlappingHeroPtr = PlayerHero; 
+
+        UE_LOG(LogTemp, Log, TEXT("Hazard %s: Player %s entered."), *GetNameSafe(this), *PlayerHero->GetName());
+
+		
+		if (bDamageOnInitialOverlap)
+		{
+            UE_LOG(LogTemp, Verbose, TEXT("Hazard %s: Applying initial damage."), *GetNameSafe(this));
+			ApplyPeriodicDamage(); 
+		}
+
+		
+        
+        const float InitialDelay = bDamageOnInitialOverlap ? DamageInterval : 0.0f;
+
+        if (DamageInterval > 0) 
+        {
+            GetWorldTimerManager().SetTimer(
+                DamageTimerHandle,         
+                this,                      
+                &AHazardousActorBase::ApplyPeriodicDamage, 
+                DamageInterval,            
+                true,                      
+                InitialDelay               
+            );
+             UE_LOG(LogTemp, Verbose, TEXT("Hazard %s: Damage timer started with interval %.2f and initial delay %.2f."), *GetNameSafe(this), DamageInterval, InitialDelay);
+        }
+        else
+        {
+             UE_LOG(LogTemp, Warning, TEXT("Hazard %s: DamageInterval is zero or negative, timer not started."), *GetNameSafe(this));
+        }
+	}
+     else if (PlayerHero && OverlappingHeroPtr.IsValid())
+     {
+         
+         UE_LOG(LogTemp, Warning, TEXT("Hazard %s: Another player %s overlapped while %s is already tracked? Ignoring."), *GetNameSafe(this), *PlayerHero->GetName(), *OverlappingHeroPtr->GetName());
+     }
+}
+
+void AHazardousActorBase::OnHazardOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    
+	APaperZDCharacter_SpriteHero* PlayerHero = Cast<APaperZDCharacter_SpriteHero>(OtherActor);
+
+    
+	if (PlayerHero && PlayerHero == OverlappingHeroPtr.Get())
+	{
+        UE_LOG(LogTemp, Log, TEXT("Hazard %s: Player %s exited."), *GetNameSafe(this), *PlayerHero->GetName());
+
+		
+		OverlappingHeroPtr.Reset();
+
+		
+		GetWorldTimerManager().ClearTimer(DamageTimerHandle);
+        UE_LOG(LogTemp, Verbose, TEXT("Hazard %s: Damage timer cleared."), *GetNameSafe(this));
+	}
+}
+
+void AHazardousActorBase::ApplyPeriodicDamage()
+{
+	
+	if (!OverlappingHeroPtr.IsValid())
+	{
+        
+        if (DamageTimerHandle.IsValid()) 
+        {
+             UE_LOG(LogTemp, Warning, TEXT("Hazard %s: ApplyPeriodicDamage called but OverlappingHeroPtr is invalid. Stopping timer."), *GetNameSafe(this));
+		    GetWorldTimerManager().ClearTimer(DamageTimerHandle);
+        }
+		return;
+	}
+
+	APaperZDCharacter_SpriteHero* PlayerHero = OverlappingHeroPtr.Get();
+
+	
+	if (!IsValid(PlayerHero))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Hazard %s: PlayerHero pointer was valid but became null unexpectedly! Stopping timer."), *GetNameSafe(this));
+		OverlappingHeroPtr.Reset();
+		GetWorldTimerManager().ClearTimer(DamageTimerHandle);
+		return;
+	}
+
+
+	
+	if (PlayerHero->GetClass()->ImplementsInterface(UDamageable::StaticClass()))
+	{
+		UE_LOG(LogTemp, Verbose, TEXT("Hazard %s: Applying %.2f damage to %s via IDamageable."), *GetNameSafe(this), DamageAmount, *PlayerHero->GetName());
+
+		
+        
+        
+		FHitResult EmptyHit; 
+		IDamageable::Execute_ApplyDamage(PlayerHero, DamageAmount, this, nullptr, EmptyHit);
+        
+	}
+	else
+	{
+        
+		UE_LOG(LogTemp, Error, TEXT("Hazard %s: Target player %s does NOT implement IDamageable! Cannot apply damage."), *GetNameSafe(this), *PlayerHero->GetName());
+		
+        OverlappingHeroPtr.Reset();
+		GetWorldTimerManager().ClearTimer(DamageTimerHandle);
+	}
+}
---
+++ My2DGameDesign/Public/Actors/HazardousActorBase.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/PrimitiveComponent.h"
+#include "TimerManager.h"
+#include "HazardousActorBase.generated.h"
+
+
+class UBoxComponent;
+class UPaperSpriteComponent;
+class APaperZDCharacter_SpriteHero;
+
+UCLASS(Abstract, Blueprintable)
+class MY2DGAMEDESIGN_API AHazardousActorBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AHazardousActorBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPrimitiveComponent> CollisionComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPaperSpriteComponent> SpriteComp;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard Settings")
+	float DamageAmount = 5.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard Settings",
+		meta=(ClampMin="0.01", ToolTip="Time in seconds between damage applications while overlapping."))
+	float DamageInterval = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard Settings"
+	)
+	bool bDamageOnInitialOverlap = true;
+
+
+	UPROPERTY(Transient)
+	TWeakObjectPtr<APaperZDCharacter_SpriteHero> OverlappingHeroPtr;
+
+
+	FTimerHandle DamageTimerHandle;
+
+
+	UFUNCTION()
+	virtual void OnHazardOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                                  const FHitResult& SweepResult);
+
+
+	UFUNCTION()
+	virtual void OnHazardOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                                UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	UFUNCTION()
+	virtual void ApplyPeriodicDamage();
+};
